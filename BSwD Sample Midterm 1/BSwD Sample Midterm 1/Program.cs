20357dab3953fc7000885906051dd47802d2126e
Kevin <kevin005005@gmail.com>
2024-03-23 22:55:11 +0100

    BSwD empty sample midterm project added

---
+++ BSwD Sample Midterm 1/BSwD Sample Midterm 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BSwD_Sample_Midterm_1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
